514a231336eb6f95d6c70e39031666d405c7dbc5
tanhui <tanhui@corp-ci.com>
2021-05-14 18:00:43 +0800

    local dev fix

--- shared/helpers/apps.js
+++ shared/helpers/apps.js
@@ -1,12 +1,12 @@
 const { hash: namehash } = require('eth-ens-namehash')
 
 const APPS = [
-  { name: 'agent', contractName: 'Agent' },
-  { name: 'vault', contractName: 'Vault' },
-  { name: 'voting', contractName: 'Voting' },
-  { name: 'survey', contractName: 'Survey' },
-  { name: 'payroll', contractName: 'Payroll' },
-  { name: 'finance', contractName: 'Finance' },
+  { name: 'agent', contractName: 'Agent' , contentURI: 'QmXCZCh1SzGLPHtJPJfQkR4u9fENhw6qGEUPR4RUTVGwxR'},
+  { name: 'vault', contractName: 'Vault' , contentURI: 'QmYZpZ7tNzHmcqZbALcMxp53Tzub3P7NNgitoMa92D6gbD'},
+  { name: 'voting', contractName: 'Voting', contentURI: 'Qmccgx6B4GyBv1RmaY2r9Tymj32ZVerKDAw6thbT3rsK1y'},
+  { name: 'survey', contractName: 'Survey' , contentURI: ''},
+  { name: 'payroll', contractName: 'Payroll' , contentURI: ''},
+  { name: 'finance', contractName: 'Finance', contentURI: 'QmRAJxYyiWz4HqLoUWpwCfouh3v3CUp8VzF7GLrWVzSoFE' },
   { name: 'token-manager', contractName: 'TokenManager' },
 ]
 
